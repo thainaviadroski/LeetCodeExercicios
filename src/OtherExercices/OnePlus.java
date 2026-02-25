d9b2f7acff1a8e2377b7b06b952ba1cbabb334b9
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2026-02-25 07:56:35 -0300

    Resolução do exercio Plus One do leetcode

---
+++ src/OtherExercices/OnePlus.java
@@ -0,0 +1,22 @@
+package OtherExercices;
+
+public class OnePlus {
+
+    static void main() {
+        int[] digits = {1, 2, 3};
+        System.out.println(plusOne(digits).toString());
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
